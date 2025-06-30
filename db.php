7a329368bfec7aba1528c8bcbf7c9ae48b4069e7
egorik078 <ihtiyarhaytekow5@gmail.com>
2025-06-30 17:07:38 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,13 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE,
-    referer VARCHAR(50),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+<?php
+$host = "db.be-mons1.bengt.wasmernet.com";
+$dbname = "ref_table";
+$username = "27d6a8807cc680007d9c8eeb04c9";
+$password = "068627d6-a880-7e7d-8000-7a764a721464";
+
+try {
+    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Bazaga ulanishda xatolik: " . $e->getMessage());
+}
+?>
