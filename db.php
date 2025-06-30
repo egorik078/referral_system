7e79dc43ab049c11267d4b898785b48658463f1d
egorik078 <ihtiyarhaytekow5@gmail.com>
2025-06-30 17:37:13 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "db.be-mons1.bengt.wasmernet.com";
-$dbname = "ref_table";
-$username = "27d6a8807cc680007d9c8eeb04c9";
-$password = "068627d6-a880-7e7d-8000-7a764a721464";
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$port = getenv("DB_PORT") ?: 3306;
 
 try {
-    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Bazaga ulanishda xatolik: " . $e->getMessage());
